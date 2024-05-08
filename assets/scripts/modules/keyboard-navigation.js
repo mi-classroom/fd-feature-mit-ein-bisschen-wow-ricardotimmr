40fda4d206fbfca0b6bfc192d114ea9f092f1f64
ricardotimmr <ricardo.timmr@gmail.com>
2024-05-08 15:34:46 +0200

    Assignment bis zum Horizontal Scroll

--- assets/scripts/modules/keyboard-navigation.js
+++ assets/scripts/modules/keyboard-navigation.js
@@ -2,9 +2,65 @@
 ---------------------------------------------------------------------------- */
 
 const addKeyboardEvents = () => {
-  
-};
+  const sections = document.querySelectorAll("main > section");
+  const lis = document.querySelectorAll("main > section > ul > li");
+  let currentSection = 0;
+  let currentLi = 0;
 
+  const jump = (direction) => {
+    if (direction === "up" && currentSection > 0) {
+      currentSection--;
+    }
+
+    if (direction === "down" && currentSection < sections.length - 1) {
+      currentSection++;
+    }
+
+    if (direction === "right" && currentLi > 0) {
+      currentLi--;
+    }
+
+    if (direction === "left" && currentLi < lis.length - 1) {
+      currentLi++;
+    }
+
+    lis[currentLi].scrollIntoView({ behavior: "smooth" });
+    sections[currentSection].scrollIntoView({ behavior: "smooth" });
+  };
+
+  document.addEventListener("keydown", (event) => {
+
+    if (event.key === "ArrowUp") {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log("Up Arrow key pressed");
+        jump("up");
+    }
+
+    if (event.key === "ArrowDown") {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log("Down Arrow key pressed");
+        jump("down");
+    }
+    
+    if (sections[currentSection].id === "atmosphere") {
+        console.log("Image strip section is in view");
+        if (event.key === "ArrowLeft") {
+            event.preventDefault();
+            event.stopPropagation();
+            console.log("Left Arrow key pressed");
+            jump("left");
+        }
+        if (event.key === "ArrowRight") {
+            event.preventDefault();
+            event.stopPropagation();
+            console.log("Right Arrow key pressed");
+            jump("right");
+        }
+    }
+  });
+};
 
 /* Exports
 ############################################################################ */
--- assets/scripts/modules/observer.js
+++ assets/scripts/modules/observer.js
@@ -2,8 +2,26 @@
 ---------------------------------------------------------------------------- */
 
 const addObserver = () => {
-
-}
+    const observedElements = document.querySelectorAll("[data-js-observe]");
+    console.log(observedElements);
+    const options = {rootMargin: "0px", threshold: 0.5};
+    
+    const observer = new IntersectionObserver (function (entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                console.log(`Element ${entry.target} is intersecting`);
+                entry.target.classList.add("is-in-view");
+            } else {
+                console.log(`Element ${entry.target} is not intersecting`);
+                entry.target.classList.remove("is-in-view");
+            }
+        });
+    }, options);
+    
+    observedElements.forEach ( function (observedElement) {
+        observer.observe (observedElement);
+    });
+};
 
 
 /* Exports
